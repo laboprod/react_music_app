4590a32ea3bf4548311f9e0d3930b342d32479f7
laboprod <labeau.francois@gmail.com>
2020-11-25 11:35:03 +0100

    import music and play/pause

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles/app.scss';
 
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library';
+
+import data from './util';
 
 function App() {
+	const [songs, setSongs] = useState(data());
+	const [currentSong, setCurrentSong] = useState(songs[0]);
+	const [isPlaying, setIsPlaying] = useState(false);
+
 	return (
 		<div className='App'>
-			<Song />
-			<Player />
+			<Song currentSong={currentSong} />
+			<Player isPlaying={isPlaying} setIsPlaying={setIsPlaying} currentSong={currentSong} />
+			<Library songs={songs} />
 		</div>
 	);
 }
