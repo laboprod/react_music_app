817ab83bf8fdc5baf0f84d68f655a26acb5b6cac
laboprod <labeau.francois@gmail.com>
2020-11-27 08:58:16 +0100

    add library button

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './styles/app.scss';
 import Player from './components/Player';
 import Song from './components/Song';
 import Library from './components/Library';
+import Nav from './components/Nav';
 
 import data from './util';
 
@@ -17,6 +18,8 @@ function App() {
 		currentTime: 0,
 		duration: 0,
 	});
+	const [libraryStatus, setLibraryStatus] = useState(false);
+
 	const timeUpdateHandler = (e) => {
 		const current = e.target.currentTime;
 		const duration = e.target.duration;
@@ -25,6 +28,7 @@ function App() {
 
 	return (
 		<div className='App'>
+			<Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
 			<Song currentSong={currentSong} />
 			<Player
 				audioRef={audioRef}
@@ -34,7 +38,14 @@ function App() {
 				setSongInfo={setSongInfo}
 				songInfo={songInfo}
 			/>
-			<Library audioRef={audioRef} songs={songs} setCurrentSong={setCurrentSong} isPlaying={isPlaying} setSongs={setSongs} />
+			<Library
+				audioRef={audioRef}
+				songs={songs}
+				setCurrentSong={setCurrentSong}
+				isPlaying={isPlaying}
+				setSongs={setSongs}
+				libraryStatus={libraryStatus}
+			/>
 			<audio onTimeUpdate={timeUpdateHandler} onLoadedMetadata={timeUpdateHandler} ref={audioRef} src={currentSong.audio}></audio>
 		</div>
 	);
